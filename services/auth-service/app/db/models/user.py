4366568aae1dc7386b7cffe69981edee816ff164
AntonNaibayer <anton.naubayer@gmail.com>
2026-08-06 23:13:46 +0700

    feat(auth-service): add db model User

---
+++ services/auth-service/app/db/models/user.py
@@ -0,0 +1,31 @@
+import uuid
+from datetime import datetime
+
+from db.base import Base
+from sqlalchemy import DateTime, func, sql
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        primary_key=True, 
+        default=uuid.uuid4,
+    )
+
+    email: Mapped[str] = mapped_column(
+        unique=True
+    )
+
+    hashed_password: Mapped[bytes] = mapped_column()
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+    )
+
+    is_active: Mapped[bool] = mapped_column(
+        default=True,
+        server_default=sql.true()
+    )
